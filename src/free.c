dade88c2a58f203b0c4371518155fcd16dbdbe94
Gabriel Guiet-Dupré <gabriel.guiet-dupre@epitech.eu>
2023-11-15 14:33:19 +0100

    ADD: kahn algo pour tri topologique

---
+++ src/free.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include "header.h"
+
+void freeWorkStation(assemblyLine_t *line)
+{
+    maillon_t *tmpMaillon, *tmpMaillon2;
+    bool endToFree = false;
+
+    for (int i = 0; i < line->nbOpe; i++) {
+        if (line->workStation[i] == NULL)
+            endToFree = true;
+        if (!endToFree) {
+            tmpMaillon = line->workStation[i]->ope;
+            while (tmpMaillon != NULL) {
+                tmpMaillon2 = tmpMaillon->next;
+                free(tmpMaillon);
+                tmpMaillon = tmpMaillon2;
+            }
+        }
+        free(line->workStation[i]);
+    }
+    free(line->workStation);
+}
+
+void freeGraph(graphe_t *graphe)
+{
+    arc_t *tmpArc, *tmpArc2;
+
+    for (int i = 0; i < graphe->ordre; i++) {
+        tmpArc = graphe->sommets[i]->arc;
+        while (tmpArc != NULL)  {
+            tmpArc2 = tmpArc->next;
+            free(tmpArc);
+            tmpArc = tmpArc2;
+        }
+        free(graphe->sommets[i]);
+    }
+    free(graphe->sommets);
+    free(graphe);
+}
+
+void freeAssemblyLine(assemblyLine_t *line)
+{
+    maillon_t *tmpMaillon, *tmpMaillon2;
+    bool endToFree = false;
+
+    for (int i = 0; i < line->nbOpe; i++) {
+        if (line->workStation[i] == NULL)
+            endToFree = true;
+        if (!endToFree) {
+            tmpMaillon = line->workStation[i]->ope;
+            while (tmpMaillon != NULL) {
+                tmpMaillon2 = tmpMaillon->next;
+                free(tmpMaillon);
+                tmpMaillon = tmpMaillon2;
+            }
+        }
+        free(line->workStation[i]);
+        free(line->ope[i]);
+        free(line->unassociable[i]);
+    }
+    free(line->workStation);
+    free(line->ope);
+    freeGraph(line->graph);
+    free(line->unassociable);
+    
+    free(line);
+}
---
+++ src/kahnAlgorithm.c
@@ -0,0 +1,138 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "header.h"
+
+ope_t *getOpeById(ope_t **ope, int id)
+{
+    for (int i = 0; ope[i] != NULL; i++)
+        if (ope[i]->id == id)
+            return ope[i];
+
+    printf("Exit Failure: operation with ID: %d doesn't exist\n", id);
+    exit(EXIT_FAILURE);
+}
+
+maillon_t *removeMaillon(maillon_t *file)
+{
+    maillon_t *tmpMaillon = file;
+
+    file = file->next;
+    free(tmpMaillon);
+
+    return file;
+}
+
+int isAssociable(maillon_t *file, assemblyLine_t *line, ope_t *currOpe, bool isSortByTime, int currWS)
+{
+    maillon_t *tmpMaillon = file;
+
+    while (tmpMaillon != NULL) {
+        if (line->unassociable[tmpMaillon->ope->id - 1][currOpe->id - 1])
+            return 0;
+        tmpMaillon = tmpMaillon->next;
+    }
+
+    tmpMaillon = line->workStation[currWS]->ope;
+    while (isSortByTime && tmpMaillon != NULL) {
+        if (line->unassociable[tmpMaillon->ope->id - 1][currOpe->id - 1])
+            return 0;
+        tmpMaillon = tmpMaillon->next;
+    }
+
+    return 1;
+}
+
+maillon_t *getSommetsWithNoDeg(assemblyLine_t *line, bool isSortByAssociable, bool isSortByTime, int *currWS)
+{
+    ope_t *tmpOpe;
+    maillon_t *file = NULL;
+    int returnIsAssociable = 0;
+    bool fileIsEmpty = true;
+
+    for (int i = 0; line->graph->sommets[i] != NULL; i++) {
+        if ((line->graph->sommets[i]->deg == 0 && !isSortByAssociable) || (line->graph->sommets[i]->deg == 0 && isSortByAssociable && (returnIsAssociable = isAssociable(file, line, getOpeById(line->ope, line->graph->sommets[i]->id), isSortByTime, *currWS)))) {
+            tmpOpe = getOpeById(line->ope, line->graph->sommets[i]->id);
+            file = addMaillon(file, tmpOpe);
+            fileIsEmpty = false;
+        }
+    }
+
+    if (isSortByAssociable && !returnIsAssociable && fileIsEmpty) {
+        *currWS += 1;
+        for (int i = 0; line->graph->sommets[i] != NULL; i++) {
+            if ((line->graph->sommets[i]->deg == 0 && !isSortByAssociable) || (line->graph->sommets[i]->deg == 0 && isSortByAssociable && (returnIsAssociable = isAssociable(file, line, getOpeById(line->ope, line->graph->sommets[i]->id), isSortByTime, *currWS)))) {
+                tmpOpe = getOpeById(line->ope, line->graph->sommets[i]->id);
+                file = addMaillon(file, tmpOpe);
+                fileIsEmpty = true;
+            }
+        }
+    }
+
+    return file;
+}
+
+double kahnAlgorithm(assemblyLine_t *line, bool isSortByTime, bool isSortByAssociable) // Algorithme de tri topologique
+{
+    int currWS = -1, len, nbStation = 0;
+    bool isFinish = false, reboot = false;
+    double currTime = 0.00, totalTime = 0.00;
+    arc_t *tmpArc;
+    ope_t *tmpOpe, **tmpArrayOpe;
+    sommet_t *tmpSommet;
+    maillon_t *file;
+
+    while (!isFinish) {
+        isFinish = true;
+        if (!isSortByTime || currWS == -1)
+            currWS++;
+
+        file = getSommetsWithNoDeg(line, isSortByAssociable, isSortByTime, &currWS);
+        if (isSortByTime)
+            sortList(&file);
+
+        while (file != NULL) {
+            isFinish = false;
+            reboot = false;
+
+            // currTime += file->ope->time;
+            if (isSortByTime && currTime + file->ope->time > line->cycleTime) { // Change de Station
+                int len = getLen(file);
+                file = getSommetsWithNoDeg(line, isSortByAssociable, isSortByTime, &currWS);
+                sortList(&file);
+                if (len == getLen(file)) {
+                    currWS++;
+                    currTime = file->ope->time;
+                } else if (currTime + file->ope->time > line->cycleTime)
+                    reboot = true;
+            } else
+                currTime += file->ope->time;
+
+            if (!reboot) {
+                line->workStation[currWS]->ope = addMaillon(line->workStation[currWS]->ope, file->ope);
+
+                tmpSommet = getSommetById(line->graph->sommets, file->ope->id);
+                tmpArc = tmpSommet->arc;
+                while (tmpArc != NULL) {
+                    tmpArc->sommet->deg -= 1;
+                    tmpArc = tmpArc->next;
+                }
+
+                tmpSommet->deg = -1;
+                file = removeMaillon(file);
+            }
+        }
+    }
+    if (!isSortByTime && isSortByAssociable)
+        line->workStation[currWS - 1] = NULL;
+    if (!isSortByTime || (isSortByAssociable && isSortByTime))
+        line->workStation[currWS] = NULL;
+    else
+        line->workStation[currWS + 1] = NULL;
+    getTimeStation(line->workStation);
+    for (int i = 0; line->workStation[i] != NULL; i++) {
+        nbStation++;
+        totalTime += line->workStation[i]->time;
+    }
+    
+    return getLoss(line, nbStation, totalTime);
+}
